d89ae5f923d2c6740e76e24ffc090143cbcdcac5
Collins Kiragu <collinskiragu@yahoo.com>
2023-06-18 18:11:15 +0300

    Gmail API Done

---
+++ config.jsx
@@ -0,0 +1,4 @@
+export const config = {
+    CLIENT_ID: '1001571243921-fp2cfsi55ftff052b6igppvl52jpop6n.apps.googleusercontent.com', // Must be taken from Google API Console (https://console.developers.google.com/)
+    API_KEY: 'AIzaSyC0yUyJRPwh6zc0GTKZThHSLrBb51XD-tk' // https://developers.google.com/maps/documentation/javascript/get-api-key
+}
